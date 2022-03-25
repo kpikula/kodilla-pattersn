899a0d09c6fd34f82375398839326cb770012cd3
kpikula <90643790+kpikula@users.noreply.github.com>
2022-03-25 20:43:45 +0100

    24.5 Adapterv2

--- src/main/java/com/kodilla/adapter/bookclassifier/librarya/BookA.java
+++ src/main/java/com/kodilla/adapter/bookclassifier/librarya/BookA.java
@@ -2,14 +2,14 @@ package com.kodilla.adapter.bookclassifier.librarya;
 
 import java.util.Objects;
 
-public class Book {
+public class BookA {
     private final String author;
     private final String title;
     private final int publicationYear;
     private final String signature;
 
-    public Book(final String author, final String title,
-                final int publicationYear, final String signature) {
+    public BookA(final String author, final String title,
+                 final int publicationYear, final String signature) {
         this.author = author;
         this.title = title;
         this.publicationYear = publicationYear;
@@ -35,13 +35,23 @@ public class Book {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Book)) return false;
-        Book book = (Book) o;
+        if (!(o instanceof BookA)) return false;
+        BookA book = (BookA) o;
         return getSignature().equals(book.getSignature());
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(getSignature());
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", publicationYear=" + publicationYear +
+                ", signature='" + signature + '\'' +
+                '}';
     }
 }
